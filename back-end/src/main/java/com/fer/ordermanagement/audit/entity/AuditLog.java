47d1622b8b55f6dea71e8656a607ec44230a4380
Fate <trantanphuc1105@gmail.com>
2026-01-19 09:15:55 +0700

    feat(audit): add audit log entity

---
+++ back-end/src/main/java/com/fer/ordermanagement/audit/entity/AuditLog.java
@@ -0,0 +1,32 @@
+package com.fer.ordermanagement.audit.entity;
+
+import com.fer.ordermanagement.auth.entity.User;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "audit_logs")
+public class AuditLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String action;
+
+    @Column(length = 50, nullable = false)
+    private String entity;
+
+    @Column(name = "entity_id",nullable = true)
+    private Long entityId;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = true)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
